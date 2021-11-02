ad8ef13c53c26371af54382b531c687159cd2cb4
alina753 <ramazanovaalina905@gmail.com>
2021-11-02 15:22:54 +0300

    Wrote CODES, replaced "•" with ".", update encrypt and decrypt

--- test/encryption/morse_test.rb
+++ test/encryption/morse_test.rb
@@ -8,25 +8,25 @@ class MorseTest < Minitest::Test
   # "hello world!"
 
   def test_morse_encrypt_a
-    assert_equal".-", Morse::encrypt('a')
+    assert_equal"._", Morse::encrypt('a')
   end
 
   def test_morse_encrypt_hello_world
-    assert_equal"•••• • •−•• •−•• −−−   •−− −−− •−• •−•• −•• −−••−− ",
+    assert_equal".... . ._.. ._.. ___   .__ ___ ._. ._.. _.. __..__ ",
                 Morse::encrypt("hello world!")
   end
 
   def test_morse_decrypt_a
-    assert_equal "a", Morse::decrypt(".-")
+    assert_equal "a", Morse::decrypt("._")
   end
 
   def test_morse_decrypt_hello_world
     assert_equal"hello world!",
-                Morse::decrypt("•••• • •−•• •−•• −−−   •−− −−− •−• •−•• −•• −−••−− ")
+                Morse::decrypt(".... . ._.. ._.. ___   .__ ___ ._. ._.. _.. __..__ ")
   end
 
   def test_morse_encrypt_hello_world_mixed_case
-    assert_equal"•••• • •−•• •−•• −−−   •−− −−− •−• •−•• −•• −−••−− ",
+    assert_equal".... . ._.. ._.. ___   .__ ___ ._. ._.. _.. __..__ ",
                 Morse::encrypt("Hello World!")
   end
 end

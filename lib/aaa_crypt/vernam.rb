9becffb13533663e01790ed2b785280583d3fdea
Alexadner <areshetnikov@sfedu.ru>
2021-11-29 23:35:22 +0300

    Added alphabet and restructured method according to it

--- lib/aaa_crypt/vernam.rb
+++ lib/aaa_crypt/vernam.rb
@@ -1,11 +1,12 @@
 module AaaCrypt
   module Vernam
 
+    ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?:;.,(){}[]"
     def self.encrypt(word, code)
       encoded_word = ''
       index = 0
       word.split('').map do |s|
-        encoded_word += (s.ord ^ code[index].ord).chr
+        encoded_word += ALPHABET[ALPHABET.index(s) ^ ALPHABET.index(code[index])]
         index += 1
       end
       encoded_word
@@ -15,9 +16,10 @@ module AaaCrypt
       decoded_word = ''
       index = 0
       encoded_word.split('').map do |s|
-        decoded_word += (s.ord ^ code[index].ord).chr
+        decoded_word += ALPHABET[ALPHABET.index(s) ^ ALPHABET.index(code[index])]
         index += 1
       end
+      decoded_word
     end
   end
 end
